a7dc9689e4fca9a0b3982ae958537a3046c64f3a
Toyotomius <Toyotomius@gmail.com>
2019-12-06 16:27:56 -0400

    Added fetching 649 main results from API. Separated Properties out to shared models.

---
+++ LotterySharperBlazorServer/Shared/Models/Lotto649Model.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace LotterySharperBlazorServer.Shared.Models
+{
+    public class Lotto
+    {
+        public List<LottoData> Lotto649 { get; set; }
+    }
+
+    public class LottoData
+    {
+        public int Bonus { get; set; }
+        public string Date { get; set; }
+        public List<int> Numbers { get; set; }
+    }
+}
