fb9432562a9952077d93ae07be0c61327fbdad50
Toyotomius <Toyotomius@gmail.com>
2019-12-09 00:06:30 -0400

    Added Lotto 649 Singles table and API call.

--- LotterySharperBlazorServer/Lotto649Service.cs
+++ LotterySharperBlazorServer/Lotto649Service.cs
@@ -1,17 +1,11 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace LotterySharperBlazorServer
 {
     public class Lotto649Service
     {
-        public HttpClient Client { get; }
-
         public Lotto649Service(HttpClient client)
         {
             client.BaseAddress = new Uri("https://localhost:44381");
@@ -19,20 +13,32 @@ namespace LotterySharperBlazorServer
             Client = client;
         }
 
+        public HttpClient Client { get; }
+
         public async Task<string> GetLotto649Async()
         {
             var request = new HttpRequestMessage(HttpMethod.Get,
                 "https://localhost:44381/api/lotto649/");
-
-
 
             var response = await Client.SendAsync(request);
 
             if (response.IsSuccessStatusCode)
             {
                 return await response.Content.ReadAsStringAsync();
-                
+            }
+            else { return null; }
+        }
 
+        public async Task<string> GetLotto649SinglesAsync()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+                "https://localhost:44381/api/lotto649/singles");
+
+            var response = await Client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
             }
             else { return null; }
         }
---
+++ LotterySharperBlazorServer/Shared/Models/LottoSinglesList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotterySharperBlazorServer.Shared.Models
+{
+  
+    public class LottoSinglesCount
+    {
+        public int First { get; set; }
+
+        public int Frequency { get; set; }
+    }
+}
