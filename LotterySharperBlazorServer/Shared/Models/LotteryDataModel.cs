32ef7a5929afd9ac729084ee810b230828de0d35
Toyotomius <Toyotomius@gmail.com>
2020-02-09 00:00:45 -0400

    Added new property to account for table not handling lists. Added justifaction option supported by most browsers.

--- LotterySharperBlazorServer/Shared/Models/LotteryDataModel.cs
+++ LotterySharperBlazorServer/Shared/Models/LotteryDataModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+
 
 namespace LotterySharperBlazorServer.Shared.Models
 {
@@ -7,5 +9,11 @@ namespace LotterySharperBlazorServer.Shared.Models
         public int Bonus { get; set; }
         public string Date { get; set; }
         public List<int> Numbers { get; set; }
+        // Crappy hack to get the blazor table to work, which can't handle lists and breaks every time I try to do this in-place.
+        public string NumbersString { get
+            {
+                                // Tabs or spaces? Spaces in this case. Eternal debate rages on.
+                return string.Join(" ", Numbers);
+            } }
     }
 }
