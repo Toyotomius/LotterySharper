b2a87aa83b833f4d020e2ef0f3cb8d318fa3b914
Toyotomius <Toyotomius@gmail.com>
2020-02-12 20:00:20 -0400

    Adjusted table page size & minor build error fix.

--- LotterySharperBlazorServer/Startup.cs
+++ LotterySharperBlazorServer/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Linq;
-using LotterySharperBlazorServer.Data;
+
 
 
 namespace LotterySharperBlazorServer
